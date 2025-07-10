9ac4c815804ded1f11b0c65d42f6f30c58b0a0ab
MukishKumar-T <mukishkumar.t2023cse@sece.ac.in>
2025-07-10 08:15:19 +0530

    Re-added EMS as regular folder

---
+++ EMS/src/main/java/com/example/springbootfirst/models/Roles.java
@@ -0,0 +1,28 @@
+package com.example.springbootfirst.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Roles {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int roleId;
+  private String name;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ EMS/src/main/java/com/example/springbootfirst/models/UserDetailsDto.java
@@ -0,0 +1,59 @@
+package com.example.springbootfirst.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsDto {
+  private String name;
+  private String userName;
+  private String email;
+  private String password;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<String> getRoleNames() {
+    return roleNames;
+  }
+
+  public void setRoleNames(Set<String> roleNames) {
+    this.roleNames = roleNames;
+  }
+
+  private Set<String> roleNames;
+}
