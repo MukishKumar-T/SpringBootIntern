7dc608fa748addce8314c4124b8c91f692fedde0
MukishKumar-T <mukishkumar.t2023cse@sece.ac.in>
2025-07-11 09:03:52 +0530

    Added Auth Controller, Methods uddateById, findByRole

--- EMS/src/main/java/com/example/springbootfirst/controllers/AuthController.java
+++ EMS/src/main/java/com/example/springbootfirst/controllers/AuthController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+
+//Authentication
 @RestController
 @RequestMapping("/employee")
 public class AuthController {
