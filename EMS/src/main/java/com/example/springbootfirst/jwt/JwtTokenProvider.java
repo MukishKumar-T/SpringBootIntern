b899ce4f18c11548bff24420ef6436e2e465a0a5
MukishKumar-T <mukishkumar.t2023cse@sece.ac.in>
2025-07-15 07:59:54 +0530

    Added finByEmpId, usersByRole methods

--- EMS/src/main/java/com/example/springbootfirst/jwt/JwtTokenProvider.java
+++ EMS/src/main/java/com/example/springbootfirst/jwt/JwtTokenProvider.java
@@ -15,42 +15,42 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    @Value("${app.jwt-secret}")
-    private String jwtSecret;
-    @Value("${app.jwt-expiration-milliseconds}")
-    private long jwtExpirationMilliSeconds;
-    private Key secretKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-    }
-    public String generateToken(Authentication authenticate){
-        UserDetails userPricipal = (UserDetails) authenticate
-                .getPrincipal();
-
-        return Jwts.builder()
-                .setSubject(userPricipal.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+jwtExpirationMilliSeconds))
-                .signWith(secretKey(),SignatureAlgorithm.HS256)
-                .compact();
-    }
-    public String getUsernameFromToken(String token){
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-    public boolean validateToken(String token){
-        try{
-            Jwts.parserBuilder()
-                    .setSigningKey(secretKey())
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e){
-            e.printStackTrace();
-        }
-        return false;
-    }
+//    @Value("${app.jwt-secret}")
+//    private String jwtSecret;
+//    @Value("${app.jwt-expiration-milliseconds}")
+//    private long jwtExpirationMilliSeconds;
+//    private Key secretKey() {
+//        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+//    }
+//    public String generateToken(Authentication authenticate){
+//        UserDetails userPricipal = (UserDetails) authenticate
+//                .getPrincipal();
+//
+//        return Jwts.builder()
+//                .setSubject(userPricipal.getUsername())
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis()+jwtExpirationMilliSeconds))
+//                .signWith(secretKey(),SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//    public String getUsernameFromToken(String token){
+//        return Jwts.parserBuilder()
+//                .setSigningKey(secretKey())
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//    public boolean validateToken(String token){
+//        try{
+//            Jwts.parserBuilder()
+//                    .setSigningKey(secretKey())
+//                    .build()
+//                    .parseClaimsJws(token);
+//            return true;
+//        } catch (JwtException | IllegalArgumentException e){
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
 }
--- EMS/src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
+++ EMS/src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RegisterDetailsRepository extends JpaRepository<RegisterDetails,Integer> {
-//    Optional<RegisterDetails> findByUserName(String userName);
+    Optional<RegisterDetails> findByUserName(String userName);
 }
