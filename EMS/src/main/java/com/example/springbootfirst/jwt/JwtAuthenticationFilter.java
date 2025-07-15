51b88dfa985e7dca684efdeb886360234975d46f
MukishKumar-T <mukishkumar.t2023cse@sece.ac.in>
2025-07-15 08:01:17 +0530

    JWT Setup

--- EMS/src/main/java/com/example/springbootfirst/jwt/JwtAuthenticationFilter.java
+++ EMS/src/main/java/com/example/springbootfirst/jwt/JwtAuthenticationFilter.java
@@ -17,34 +17,34 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    @Autowired
-//    CustomUserDetailsService userDetailsService;
-//    @Autowired
-//    JwtTokenProvider jwtTokenProvider;
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,
-//                                    HttpServletResponse response,
-//                                    FilterChain filterChain) throws ServletException,
-//            IOException {
-//        String authHeader = request.getHeader("Authorization");
-//        String token = null;
-//        String username = null;
-//        if(authHeader != null && authHeader.startsWith("Bearer ")){
-//            token = authHeader.substring(7);
-//            username = jwtTokenProvider.getUsernameFromToken(token);
-//        }
-//        if(username != null &&
-//                SecurityContextHolder.getContext().getAuthentication() == null){
-//            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//            if(jwtTokenProvider.validateToken(token)){
-//                UsernamePasswordAuthenticationToken authToken =
-//                        new UsernamePasswordAuthenticationToken(
-//                                userDetails,null,userDetails.getAuthorities());
-//                authToken.setDetails(
-//                        new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(authToken);
-//            }
-//        }
-//        filterChain.doFilter(request,response);
-//    }
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException,
+            IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            token = authHeader.substring(7);
+            username = jwtTokenProvider.getUsernameFromToken(token);
+        }
+        if(username != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if(jwtTokenProvider.validateToken(token)){
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
 }
